cd40cb49cb106afd54e26ebdbdf10b16eefd3c5d
caifei yang <caifei yang>
2017-02-13 22:46:05 +0800

    define const values

---
+++ Defines.h
@@ -0,0 +1,22 @@
+
+#define WINDOW_WIDTH		800			// 窗口宽度
+#define WINDOW_HEIGHT		600			// 窗口高度
+#define WINDOW_CAPTION		"Paddle"	// 窗口名称
+
+// 定义桨的速度
+#define PLAYER_SPEED		10			// 玩家浆速
+#define COMPUTER_SPEED		10			// 电脑浆速
+
+// 定义球的速度
+#define BALL_SPEED_MODIFIER	5			// 将浆距中心的距离除以5, 得到我们想要的速度
+#define BALL_SPEED_Y		10			// 球垂直移动的速度恒定
+
+// 定义在屏幕上显示分数的位置
+#define COMPUTER_SCORE_X	10			// 计算机得分位置
+#define COMPUTER_SCORE_Y	10
+#define PLAYER_SCORE_X		150			// 玩家得分位置
+#define PLAYER_SCORE_Y		10
+
+// 定义游戏中浆的位置
+#define COMPUTER_Y			30			// 电脑玩家浆的位置
+#define PLAYER_Y			550			// 玩家浆的位置
