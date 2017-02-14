f0f61dee44e10a471847552f196a944a3992b48b
caifei yang <caifei yang>
2017-02-14 12:32:26 +0800

    add handle ball collision with paddle, finished

--- Window.cpp
+++ Window.cpp
@@ -5,21 +5,31 @@
 
 
 Window::Window(void):
-	win(nullptr), renderer(nullptr)
+	win(nullptr), renderer(nullptr), Font(nullptr)
 {
-	IsStarted = true;
+	IsStarted	  =  true;
+	Pause		  =  false;
+	PlayerScore	  =  0;
+	ComputerScore =  0;
 	// 初始化SDL
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		cout << "SDL_Init Error: " << SDL_GetError() << endl;
 		exit(1);
 	}
+	// 初始化图像库
 	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
+	// 初始化字体库
+	TTF_Init();
+	Font = TTF_OpenFont("res/SourceSansPro-Regular.ttf", 20);
 
 	// 创建窗口
 	win = SDL_CreateWindow(WINDOW_CAPTION, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 	// 创建渲染器(使用硬件加速渲染和与当前显示器同步)
 	renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	// 初始化随机数种子
+	srand(unsigned int (time(0)));
 
 	// 加载资源文件
 	LoadResouceFile();
@@ -29,19 +39,11 @@ Window::Window(void):
 
 void Window::Init()
 {
-	// 设置球速、初始位置等
-	Ball.setSpeed_x(2);
-	Ball.setSpeed_y(0);
-	int Position_x = (WINDOW_WIDTH / 2);
-	int Position_y = (WINDOW_HEIGHT / 2);
-	SDL_Rect rect0 = {
-		Position_x, Position_y, Ball.getGeometry().w, Ball.getGeometry().h
-	};
-	Ball.setLocation(rect0);
+	ResetGame();
 
 	// 设置电脑玩家浆速、位置等
 	PaddleC.setSpeed_x(COMPUTER_SPEED);
-	Position_x = (WINDOW_WIDTH / 2) - (PaddleC.getGeometry().w / 2);
+	int Position_x = (WINDOW_WIDTH / 2) - (PaddleC.getGeometry().w / 2);
 	SDL_Rect rect = {
 		Position_x, COMPUTER_Y, PaddleC.getGeometry().w, PaddleC.getGeometry().h
 	};
@@ -56,6 +58,19 @@ void Window::Init()
 	PaddleP.setLocation(rect);
 }
 
+void Window::ResetGame()
+{
+	// 设置球速、初始位置等
+	Ball.setSpeed_x(5);
+	Ball.setSpeed_y(-3);
+	int Position_x = (WINDOW_WIDTH / 2);
+	int Position_y = (WINDOW_HEIGHT / 2);
+	SDL_Rect rect0 = {
+		Position_x, Position_y, Ball.getGeometry().w, Ball.getGeometry().h
+	};
+	Ball.setLocation(rect0);
+}
+
 // 进入消息循环
 void Window::Start()
 {
@@ -66,13 +81,50 @@ void Window::Start()
 	{
 		if (SDL_PollEvent(&event))
 		{
-			if (event.type == SDL_QUIT)
+			int speed = 0;
+			switch (event.type)
 			{
+			case SDL_QUIT:
 				gameRunning = false;
+				break;
+			case SDL_KEYDOWN:	// 键盘按键事件
+				{
+					switch (event.key.keysym.sym)
+					{
+					case SDLK_UP:
+						break;
+					case SDLK_LEFT:
+						speed = -PLAYER_SPEED;
+						break;
+					case SDLK_RIGHT:
+						speed = PLAYER_SPEED;
+						break;
+					case SDLK_DOWN:
+						break;
+					case SDLK_SPACE:
+						{
+							Pause = !Pause;
+							if (Pause)
+							{
+								// 重置游戏
+								ResetGame();
+							}
+						}
+						break;
+					default:
+						break;
+					}
+				}
+				break;
+			default:
+				break;
 			}
+
+			PaddleP.setLocation_XY(PaddleP.getLocation().x + speed, PaddleP.getLocation().y);
 		}
 
-		MoveBall();
+		HandleBall();
+		HandleComputerPaddle();
 
 		Update();
 		Refresh();
@@ -103,6 +155,30 @@ void Window::Update()
 		SDL_RenderCopy(renderer, Ball.getTexture(), NULL, &Ball.getLocation());
 		SDL_RenderCopy(renderer, PaddleC.getTexture(), NULL, &PaddleC.getLocation());
 		SDL_RenderCopy(renderer, PaddleP.getTexture(), NULL, &PaddleP.getLocation());
+
+		// 输出电脑和玩家分数
+		char c_score[50];
+		sprintf_s(c_score, "Computer Score: %d", ComputerScore);
+
+		char p_score[50];
+		sprintf_s(p_score, "Player Score: %d", PlayerScore);
+
+		width = 0, height = 0;
+		SDL_Texture* c_textre = Draw_text(c_score);
+		SDL_QueryTexture(c_textre, NULL, NULL, &width, &height);
+		rect.x = COMPUTER_SCORE_X;
+		rect.y = COMPUTER_SCORE_Y;
+		rect.w = width;
+		rect.h = height;
+		SDL_RenderCopy(renderer, c_textre, NULL, &rect);
+
+		SDL_Texture* p_textre = Draw_text(p_score);
+		SDL_QueryTexture(p_textre, NULL, NULL, &width, &height);
+		rect.x = PLAYER_SCORE_X;
+		rect.y = PLAYER_SCORE_Y;
+		rect.w = width;
+		rect.h = height;
+		SDL_RenderCopy(renderer, p_textre, NULL, &rect);
 	}
 }
 
@@ -184,10 +260,214 @@ bool Window::CheckWallCollisions(GameObject& Object, Directions dir)
 	return false;
 }
 
+// 检测球是否会碰撞到浆
+bool Window::CheckBallCollisions(GameObject& Object)
+{
+	// 临时变量，以保持代码整洁和可读性
+	int ball_x = Ball.getLocation().x;
+	int ball_y = Ball.getLocation().y;
+	int ball_width = Ball.getLocation().w;
+	int ball_height = Ball.getLocation().h;
+	int ball_speed = Ball.getSpeed_y();
+
+	int paddle_x = Object.getLocation().x;
+	int paddle_y = Object.getLocation().y;
+	int paddle_width = Object.getLocation().w;
+	int paddle_height = Object.getLocation().h;
+
+	// 检测玩家的浆(通过纵坐标的位置判断)
+	if (Object.getLocation().y == PLAYER_Y)
+	{
+		// 检测球是否在玩家浆的Y范围内
+		if ((ball_speed > 0) && (ball_y + ball_height >= paddle_y) &&
+			(ball_y + ball_height <= paddle_y + paddle_height))
+		{
+			// 如果ball在浆的X范围内，返回true
+			if ((ball_x <= paddle_x + paddle_width) &&
+				(ball_x + ball_width >= paddle_x))
+			{
+				return true;
+			}
+		}
+	}
+	else  // 电脑的浆
+	{
+		// 检查球是否在电脑浆的Y范围内
+		if ((ball_speed < 0) && (ball_y >= paddle_y) &&
+			(ball_y <= paddle_y + paddle_height))
+		{
+			// 如果球在浆的x范围内，返回true
+			if ((ball_x <= paddle_x + paddle_width) &&
+				(ball_x + ball_width >= paddle_x))
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+// 移动小球，做相关碰撞事件的处理逻辑，例如如果碰到浆改变小球速度等
+void Window::HandleBall()
+{
+	// 开始移动小球
+	MoveBall();
+
+	// 检测是否与玩家的浆发生碰撞，如果发生碰撞需要改变小球的速度
+	if (CheckBallCollisions(PaddleP))
+	{
+		// 获取球和浆在当前位置的中心位置
+		int ball_center = Ball.getLocation().x + Ball.getLocation().w / 2;
+		int paddle_center = PaddleP.getLocation().x + PaddleP.getLocation().w / 2;
+
+		// ball_center与paddle_center的相对距离
+		int distance = ball_center - paddle_center;
+
+		// 根据距浆中心的距离增加x速度
+		Ball.setSpeed_x(distance / BALL_SPEED_MODIFIER);
+		Ball.setSpeed_y(-Ball.getSpeed_y());
+	}
+
+	// 检测是否与电脑的浆发生碰撞，如果发生碰撞改变小球的速度
+	if (CheckBallCollisions(PaddleC))
+	{
+		int ball_center = Ball.getLocation().x + Ball.getLocation().w / 2;
+		int paddle_center = PaddleC.getLocation().x + PaddleC.getLocation().w / 2;
+
+		int distance = ball_center - paddle_center;
+
+		Ball.setSpeed_x(distance / BALL_SPEED_MODIFIER);
+		Ball.setSpeed_y(-Ball.getSpeed_y());
+	}
+
+	// 处理小球Y坐标小于0或大于WINDOW_HEIGHT时的逻辑
+	if (Ball.getLocation().y < 0)
+	{
+		// 处理玩家
+		HandlePlayerScore();
+	}
+	if ((Ball.getLocation().y + Ball.getLocation().h) > WINDOW_HEIGHT)
+	{
+		// 处理电脑玩家
+		HandleComputerScore();
+	}
+}
+
+// 处理电脑玩家的浆，使之智能化
+/*
+选择一个介于1和3之间的随机数。1表示浆的左边，2表示中心，3表示右边，计算机做出关于那部分击球随机决定
+检测小球是否改变了速度。如果速度改变，就应该做出新的决策，否则，只是继续将计算机的浆向球移动，直到浆的适当部分在球的路径
+中或者球再次改变方向
+*/
+void Window::HandleComputerPaddle()
+{
+	// 第一次调用这个函数时，记录球的当前速度，并决定浆的那部分击球
+	static int last_speed = Ball.getSpeed_x();
+	static int decision = rand() % 3 + 1;
+
+	int computer_x;
+	int ball_center = Ball.getLocation().x + Ball.getLocation().w / 2;
+
+	// 检测小球是否改变了方向
+	if (last_speed != Ball.getSpeed_x())
+	{
+		// 如果小球速度（x）方向发生了改变，重新决策
+		decision = rand() % 3 + 1;
+		last_speed = Ball.getSpeed_x();
+	}
+
+	// 根据决策设置浆的x坐标
+	switch (decision)
+	{
+	case 1:		// 左
+		computer_x = PaddleC.getLocation().x;
+		break;
+	case 2:		// 中
+		computer_x = PaddleC.getLocation().x + PaddleC.getLocation().w / 2;
+		break;
+	case 3:		// 右
+		computer_x = PaddleC.getLocation().x + PaddleC.getLocation().w ;
+		break;
+	default:
+		break;
+	}
+
+	// 防止计算机快速来回移动
+	if (abs(computer_x - ball_center) < 10)
+		return ;
+
+	// 球在浆的左边
+	if (computer_x > ball_center)
+	{
+		// 确保没有碰到屏幕的左边
+		if (!(CheckWallCollisions(PaddleC, LEFT)))
+			PaddleC.setLocation_XY(PaddleC.getLocation().x - COMPUTER_SPEED, PaddleC.getLocation().y);
+	}
+	// 球在浆的右边
+	if (computer_x < ball_center)
+	{
+		// 确保没有碰到屏幕的右边
+		if (!(CheckWallCollisions(PaddleC, RIGHT)))
+			PaddleC.setLocation_XY(PaddleC.getLocation().x + COMPUTER_SPEED, PaddleC.getLocation().y);
+	}
+}
+
+// 处理玩家分数
+void Window::HandlePlayerScore()
+{
+	// Increase score
+	PlayerScore++;
+	
+	// Reset ball
+	Ball.setSpeed_x(0);
+	Ball.setSpeed_y(0);
+	Ball.setLocation_XY((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2));
+
+	// 检查玩家是否赢了
+	if (PlayerScore == 10)
+	{
+		ComputerScore = 0;
+		PlayerScore = 0;
+	}
+}
+
+// 处理电脑分数
+void Window::HandleComputerScore()
+{
+	// Increase score
+	ComputerScore++;
+
+	// Reset ball
+	Ball.setSpeed_x(0);
+	Ball.setSpeed_y(0);
+	Ball.setLocation_XY((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2));
+
+	// 检查玩家是否赢了
+	if (ComputerScore == 10)
+	{
+		ComputerScore = 0;
+		PlayerScore = 0;
+	}
+}
+
 ///////////////////////////////////////////////////////////////
+
+SDL_Texture* Window::Draw_text(std::string message)
+{
+	SDL_Color color = { 0, 0, 0 };
+	SDL_Surface* surface = TTF_RenderText_Blended(Font, message.c_str(), color);
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+	SDL_FreeSurface(surface);
+
+	return texture;
+}
 
 Window::~Window(void)
 {
+	TTF_CloseFont(Font);
+	TTF_Quit();
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
--- Window.h
+++ Window.h
@@ -5,7 +5,10 @@
 */
 
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <iostream>
+#include <time.h>
+#include <math.h>
 #include "GameObject.h"
 #include "Defines.h"
 #include "Enums.h"
@@ -25,21 +28,40 @@ public:
 	void LoadResouceFile();		// 加载资源文件
 	SDL_Texture* Load_image(std::string FilePath);	// 加载图像
 	void Init();				// 初始化游戏对象
+	void ResetGame();			// 重置游戏
+	SDL_Texture* Draw_text(std::string message);	// 绘制文字
 
 // 游戏逻辑
 public:
+	// 移动小球
 	void MoveBall();
+	// 检查球是否会碰到Wall
 	bool CheckWallCollisions(GameObject& Object, Directions dir);
+	// 检查球是否会碰撞到浆
+	bool CheckBallCollisions(GameObject& Object);
+	// 移动小球，做相关碰撞事件的处理逻辑，例如如果碰到浆改变小球速度等
+	void HandleBall();
+	// 处理电脑的浆，使之智能
+	void HandleComputerPaddle();
+	// 处理玩家分数
+	void HandlePlayerScore();
+	// 处理电脑分数
+	void HandleComputerScore();
 
 private:
 	SDL_Window* win;			// 窗口
 	SDL_Renderer* renderer;		// 渲染器
 	SDL_Texture* bg;			// 背景纹理
+	TTF_Font* Font;				// 字体
 
 	GameObject Ball;			// 小球
 	GameObject PaddleC;			// 板（电脑）
 	GameObject PaddleP;			// 板（玩家）
 
 	bool IsStarted;				// 游戏是否开始
+	bool Pause;					// 暂停游戏
+
+	int PlayerScore;			// 玩家分数
+	int ComputerScore;			// 电脑分数
 };
 
